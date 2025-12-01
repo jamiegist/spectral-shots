714046aa196420ba70654f534b85bcf62b7d8563
Jamie Gist <jamie.gist.7@gmail.com>
2025-11-30 17:54:20 -0800

    Added the carousel feature for images. Need to fix nav and page styling and add contact form.

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+const swiper = new Swiper('.card-wrapper', {
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+  loop: true,
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    },
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+});
